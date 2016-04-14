4130bed342ec3d094acf1422ca1e75a288a36776
Tom Morton <tomm@riseup.net>
2016-04-14 14:25:50 +0100

    Kotlin + Gradle hello world

---
+++ README.md
@@ -0,0 +1,8 @@
+Simple Kotlin hello world (with gradle build tool)
+==================================================
+
+Just run:
+gradle run
+
+and:
+gradle test
---
+++ src/test/kotlin/LibraryTest.kt
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import kotlin.test.assertTrue;
+
+class MyTestCase {
+    @Test fun testSomething() {
+        assertTrue(true, "something should be true!");
+    }
+}
